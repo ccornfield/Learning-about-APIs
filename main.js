036faf46d9ea1470aa307a4c5ee12ee34d3ff87b
ecolus <ecolus48@gmail.com>
2024-06-09 14:51:28 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,16 @@
+var xhr = new XMLHttpRequest();
+var data;
+
+xhr.open('GET', 'https://ci-swapi.herokuapp.com/api/');
+xhr.send();
+
+xhr.onreadystatechange = function() {
+    console.log(this.readyState);
+    if (this.readyState == 4 && this.status == 200) {
+        data = JSON.parse(this.responseText)
+    }
+};
+
+setTimeout(function() {
+    console.log(data);
+}, 500);
